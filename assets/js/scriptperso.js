508c916909a7f84ad7cdbfc4a2858fdf15029fe6
shumez <stanis@netvolution.fr>
2020-12-04 11:43:11 +0100

    Suppression des "alert" dans la procédure de connexion / déconnexion

--- assets/js/scriptperso.js
+++ assets/js/scriptperso.js
@@ -4,11 +4,9 @@ let loggedin = false;
 login.addEventListener("click", () => {
     if (!loggedin) {
         loggedin = true;
-        alert("Bienvenue");
         login.innerText = "Déconnexion";
     } else {
         loggedin = false;
-        alert("Vous êtes déconnecté");
         login.innerText = "Connexion";
     }
 });
